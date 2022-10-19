18918af62fbaf1fa853c7cd715c7cbf299438242
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-10-19 12:49:54 -0500

    setup some files and mod code

---
+++ server/schemas/index.js
@@ -0,0 +1,5 @@
+// from mod 
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+module.exports = { typeDefs, resolvers };
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,103 @@
+// from mod see reslovers and challenge docs 
+
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+`;
+
+// // export the typeDefs
+// module.exports = typeDefs;
+
+
+// typeDefs.js: Define the necessary Query and Mutation types:
+
+// Query type:
+
+// me: Which returns a User type.
+// Mutation type:
+
+// login: Accepts an email and password as parameters; returns an Auth type.
+
+// addUser: Accepts a username, email, and password as parameters; returns an Auth type.
+
+// saveBook: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type. (Look into creating what's known as an input type to handle all of these parameters!)
+
+// removeBook: Accepts a book's bookId as a parameter; returns a User type.
+
+// User type:
+
+// _id
+
+// username
+
+// email
+
+// bookCount
+
+// savedBooks (This will be an array of the Book type.)
+
+// Book type:
+
+// bookId (Not the _id, but the book's id value returned from Google's Book API.)
+
+// authors (An array of strings, as there may be more than one author.)
+
+// description
+
+// title
+
+// image
+
+// link
+
+// Auth type:
+
+// token
+
+// user (References the User type.)
